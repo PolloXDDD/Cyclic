910f17ddece09144a10ccc84d3f77d4c9ce27a64
Kaoru Aguilera Katayama <elpollitocientifico@gmail.com>
2026-09-14 12:53:02 -0600

    Import actual completed zeta formalization

--- MillenniumSuite/BSD_RH.lean
+++ MillenniumSuite/BSD_RH.lean
@@ -1,5 +1,6 @@
 import MillenniumSuite.BSD_RH.ProblemStatement
 import MillenniumSuite.BSD_RH.Scattering
 import MillenniumSuite.BSD_RH.CriticalLine
+import MillenniumSuite.BSD_RH.ActualZeta
 import MillenniumSuite.BSD_RH.MaassSelbergPositivity
 import MillenniumSuite.BSD_RH.RiemannStatement
