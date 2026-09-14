617d00f1a38e1cb485b09444b7e89703dbbda4d6
Kaoru Aguilera Katayama <elpollitocientifico@gmail.com>
2026-09-14 12:53:36 -0600

    Import finite Yang-Mills action layer

--- MillenniumSuite/NS_YM.lean
+++ MillenniumSuite/NS_YM.lean
@@ -2,4 +2,5 @@ import MillenniumSuite.NS_YM.ProblemStatement
 import MillenniumSuite.NS_YM.GaugeConnection
 import MillenniumSuite.NS_YM.CorrelationLength
 import MillenniumSuite.NS_YM.KolmogorovLength
+import MillenniumSuite.NS_YM.FiniteAction
 import MillenniumSuite.NS_YM.YangMillsStatement
